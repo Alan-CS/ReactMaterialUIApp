852f1263b9767ed7e5ff0b5bf31ee3cf982b6b0a
AlanCS <AlanCS1010@gmail.com>
2020-05-11 12:12:28 -0700

    Textual and text color changes.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        {/*No text here*/}
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -25,21 +25,23 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h4" component="h1" gutterBottom color="primary">
           A Material-UI based React App
         </Typography>
-        <div>
-          <h2>
-            This app has the following features
-          </h2>
-          <ul>
-            <li> It is Create React App based </li>
-            <li> It is built using the Material-UI toolkit</li>
-            <li> It does routing using React Router </li>
-            <li> It does forms validation using Formik forms </li>
-            <li> It also uses YUP for driving forms validation logic </li>
-          </ul>
-        </div>
+        <Typography color="textSecondary">
+          <div>
+            <h3>
+              This app has the following features
+            </h3>
+            <ul>
+              <li> It is Create React App based </li>
+              <li> It is built using the Material-UI toolkit</li>
+              <li> It does routing using React Router </li>
+              <li> It does forms validation using Formik forms </li>
+              <li> It also uses YUP for driving forms validation logic </li>
+            </ul>
+          </div>
+        </Typography>
         <ProTip />
         <Copyright />
       </Box>
