05a573fd2fc3085ea979a70e8675dcd99a8af35f
AlanCS <AlanCS1010@gmail.com>
2020-05-12 11:40:02 -0700

    Reformatted ContactForm.js.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,7 @@ import TextField from '@material-ui/core/TextField';
 import {Grid, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
-import { ErrorMessage, Field, Formik, Form } from 'formik';
+import {ErrorMessage, Field, Formik, Form} from 'formik';
 import * as Yup from 'yup';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,7 +31,7 @@ export default function ContactForm() {
     name: Yup.string().required('Required'),
     email: Yup.string().email().required('Required'),
     message: Yup.string().required('Required'),
-    });
+  });
 
   const submitHandler = (values, {setSubmitting}) => {
     setTimeout(() => {
@@ -51,55 +51,55 @@ export default function ContactForm() {
           Please provide us the information below and we will get in touch with you at the earliest.
         </Typography>
 
-          <Formik
-            initialValues={initialValues}
-            onSubmit={submitHandler}
-            validationSchema={validationSchema}
-          >
-            {(props) => {
+        <Formik
+          initialValues={initialValues}
+          onSubmit={submitHandler}
+          validationSchema={validationSchema}
+        >
+          {(props) => {
 
-              const {
-                dirty,
-                errors,
-                handleBlur,
-                handleChange,
-                handleReset,
-                handleSubmit,
-                isSubmitting,
-                touched,
-                values,
-              } = props;
+            const {
+              dirty,
+              errors,
+              handleBlur,
+              handleChange,
+              handleReset,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values,
+            } = props;
 
-              return (
-                <form onSubmit={handleSubmit}>
-                  <Grid container >
-                    <Grid item xs={12} sm={6}>
-                      <TextField
-                        className={classes.input}
-                        error={errors.name && touched.name}
-                        helperText={errors.name && touched.name && errors.name}
-                        label="Your Name"
-                        margin="normal"
-                        name="name"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.name}
-                      />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField
-                        className={classes.input}
-                        error={errors.email && touched.email}
-                        helperText={errors.email && touched.email && errors.email}
-                        label="Your Email"
-                        margin="normal"
-                        name="email"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.email}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
+            return (
+              <form onSubmit={handleSubmit}>
+                <Grid container>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      className={classes.input}
+                      error={errors.name && touched.name}
+                      helperText={errors.name && touched.name && errors.name}
+                      label="Your Name"
+                      margin="normal"
+                      name="name"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.name}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      className={classes.input}
+                      error={errors.email && touched.email}
+                      helperText={errors.email && touched.email && errors.email}
+                      label="Your Email"
+                      margin="normal"
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.email}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
                     <TextField
                       className={classes.textarea}
                       error={errors.message && touched.message}
@@ -112,27 +112,27 @@ export default function ContactForm() {
                       onChange={handleChange}
                       value={values.message}
                     />
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={4}>
-                      <Button
-                        type="button"
-                        className="outline"
-                        onClick={handleReset}
-                        disabled={!dirty || isSubmitting}
-                      >
-                        Reset
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={4}>
-                      <Button color="primary" type="submit" disabled={isSubmitting}>
-                        Send Message
-                      </Button>
-                    </Grid>
                   </Grid>
-                </form>
-              );
-            }}
-          </Formik>
+                  <Grid item xs={12} sm={4} md={4}>
+                    <Button
+                      type="button"
+                      className="outline"
+                      onClick={handleReset}
+                      disabled={!dirty || isSubmitting}
+                    >
+                      Reset
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12} sm={4} md={4}>
+                    <Button color="primary" type="submit" disabled={isSubmitting}>
+                      Send Message
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            );
+          }}
+        </Formik>
       </Box>
     </Container>
   );
