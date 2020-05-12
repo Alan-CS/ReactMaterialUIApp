1d57e601e19812b9a2199058af7eca8b8a968fb0
AlanCS <AlanCS1010@gmail.com>
2020-05-11 17:27:37 -0700

    More changes to Contact Card.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,14 @@
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import CardActionArea from "@material-ui/core/CardActionArea";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
@@ -54,14 +46,10 @@ export default function ContactCard({id, image, firstname, lastname, address, ci
         title={firstname + " " + lastname}
       />
       <CardContent>
-        <Typography variant="h8" component="h2" gutterBottom>
+        <Typography variant="body2" component="p" gutterBottom>
           {address}
-        </Typography>
-        <Typography variant="h8" component="h2" gutterBottom>
-          {city}
-        </Typography>
-        <Typography variant="h8" component="h2" gutterBottom>
-          {country}
+          <br/>
+          {city} {", "} {country}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
