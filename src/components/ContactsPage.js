ee0e85b214091aa4afc0913d95df3511e2ae1c51
AlanCS <AlanCS1010@gmail.com>
2020-05-11 18:22:41 -0700

    Changes to ContactsPage.js.

--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -1,78 +1,34 @@
 import React from "react";
 
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Container from "@material-ui/core/Container";
 import {Grid, Typography} from "@material-ui/core";
 
 import Contact from "./Contact"
 import data from "../db";
 
-function ContactOld(id, image, firstname, lastname, address, city, country, email) {
-  return (
-    <Grid item key={id}>
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            height="30"
-            width="20"
-            image={image}
-            alt={firstname + " " + lastname}
-            title={firstname + " " + lastname}
-            component="img"
-          />
-          <CardContent>
-            <Typography variant="h5" component="h2" gutterBottom>
-              {firstname + " " + lastname}
-            </Typography>
-            <Typography variant="h8" component="h2" gutterBottom>
-              {address}
-            </Typography>
-            <Typography variant="h8" component="h2" gutterBottom>
-              {city}
-            </Typography>
-            <Typography variant="h8" component="h2" gutterBottom>
-              {country}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button color="primary" size="small">
-            Edit
-          </Button>
-          <Button color="primary" size="small">
-            View
-          </Button>
-          <Button color="primary" size="small">
-            Send Email
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
-}
-
 export default function Contacts() {
+
   const contacts = data.contacts || [];
   // console.log("Contacts = ", contacts);
+
+  /*
+     Below: <Box> exists only to add Y margin (as is also done on the other pages for consistency.
+     Otherwise, you can remove <Box> and possibly use style={{marginTop: '2em'}} on Container
+  */
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h5" gutterBottom color="primary">
           Contact List
         </Typography>
-        <div style={{padding: 40, marginTop: 30,}}>
-          <Grid container spacing={45} justify="center">
-            {contacts.map(contact => (
+        <Grid style={{marginTop: 20}} container spacing={4}>
+          {contacts.map(contact => (
+            <Grid item xs="12" sm="6">
               <Contact {...contact} />
-            ))}
-          </Grid>
-        </div>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Container>
   );
