156a1c02f35f3c1f9379d32480ec8d104a36cbe1
AlanCS <AlanCS1010@gmail.com>
2020-05-13 12:04:28 -0700

    Cleaned out Footer.js to remove warnings.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,16 @@ import Link from "@material-ui/core/Link";
 
 export default function Footer() {
   return (
-    <Typography style={{marginTop: '6em'}}variant="body2" color="textSecondary" align="center">
-      <hr style={{width: '80%'}}/>
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        {/*No text here*/}
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <>
+      <hr style={{width: '80%', marginTop: '6em'}}/>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Open Source
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </>
   );
 }
